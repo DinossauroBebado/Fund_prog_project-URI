a3f08c64b25e9b2b99ea75dbc7b1b8dc3569c5d4
DinossauroBebado <guipsilva32@gmail.com>
2021-04-09 19:15:10 -0300

    Runtime error

---
+++ 1046.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+////////////////////////////Runtime error
+int main(){
+
+int i,f,d;
+
+d=0;
+
+scanf("%d %d",&i,&f);
+
+if(i==f){
+    d = 24;
+}
+else if(f>i){
+    d = f-i;
+}
+else if(i>f){
+    d = (24-i) + f;
+}
+printf("O JOGO DUROU %d HORA(S)\n",d);
+
+}
