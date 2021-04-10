92a0c4013ac48f4d8304c71f00db8fa97aa512eb
DinossauroBebado <guipsilva32@gmail.com>
2021-04-10 00:14:10 -0300

    run time error

---
+++ 1864.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+char s[50] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+int n,i ;
+scanf("%d",&n);
+
+for(i=0;i<=n;i++){
+    printf("%c",s[i]);
+}
+    printf("\n");
+}
