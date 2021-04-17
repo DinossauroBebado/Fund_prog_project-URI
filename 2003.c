49fa769ccfaf25da4b818ee4342b74f45820ba5c
DinossauroBebado <guipsilva32@gmail.com>
2021-04-16 21:55:37 -0300

    ok

---
+++ 2003.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+int horas,minutos,atraso ;
+while (scanf("%d:%d", &horas, &minutos) != EOF){
+    horas = horas*60 ;
+
+    atraso = (horas + minutos + 60) - 8*60 ;
+    if(atraso<=0){
+     printf("Atraso maximo: %d\n",0) ;
+    }else{
+     printf("Atraso maximo: %d\n",atraso);
+    }
+}
+
+
+}
