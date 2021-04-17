646b7ab249c0a36a10914503ab03071fd4e8573b
DinossauroBebado <guipsilva32@gmail.com>
2021-04-16 23:09:37 -0300

    ok

---
+++ 2060.c
@@ -0,0 +1,28 @@
+int main(){
+    int n,i,num;
+    int count2,count3,count4,count5;
+    count2 = 0;
+    count3 = 0;
+    count4 = 0;
+    count5 = 0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&num);
+        printf("%d\n",num);
+        if(num%2==0){
+           count2 ++ ;
+        }
+        if(num%3==0){
+           count3++ ;
+        }
+        if(num%4==0){
+            count4++ ;
+        }
+        if(num%5==0){
+            count5++ ;
+        }
+   }
+    printf("%d Multiplo(s) de 2\n%d Multiplo(s) de 3\n%d Multiplo(s) de 4\n%d Multiplo(s) de 5\n",count2,count3,count4,count5);
+
+ return 0;
+}
